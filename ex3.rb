3fc4763b7d61a192804b552abbbf501a40688a26
krishnateja7 <krrishnateja7@gmail.com>
2017-06-10 23:44:20 -0400

    initial commit

---
+++ ex3.rb
@@ -0,0 +1,22 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30/6}" # addition.
+puts "Roosters #{100 - 25 * 3 % 4}" # percent or modulus.
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4  % 2 - 1 / 4 + 6 # ruby computations.
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7 # lesser than the sum of two.
+
+puts "What is 3 + 2? #{3 + 2}" # adding two digits.
+puts "What is 5 - 7? #{5 - 7}" # subtraction of two digits.
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" # greater than.
+puts "Is it greater or equal? #{5 >= -2}" # greater than or equal to.
+puts "Is it less or equal? #{5 <= -2}" # lesser than or equal to.
---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'kris'
+my_age = 24
+my_height = 188 # feet
+my_weight = 210 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} cms tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usally #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right.
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
